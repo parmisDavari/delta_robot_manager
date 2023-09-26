bff185c4e87d57801c6f8d52a24c0609816a9adb
Arman Barghi <arman.barghi@gmail.com>
2023-09-26 16:02:27 +0330

    delta_manager class added

---
+++ delta_manager/delta_manager.py
@@ -0,0 +1,80 @@
+import time
+import serial
+import serial.tools.list_ports as port_list
+import delta_manager.camera as Camera
+import delta_manager.client as Client
+
+
+class DeltaManager():
+    SERIAL_BAUD_RATE = 38400
+
+    def __init__(self):
+        pass
+
+    ## Gripper functions
+    def connect_gripper(self, ):
+        available_ports = self.get_all_ports()
+        for port in available_ports:
+            if 'COM' in self.get_port_name(port) || 'ttyACM' in self.get_port_name(port):  #TODO: check if this works on linux
+                self.gripper = serial.Serial(
+                    self.get_port_name(port), 
+                    SERIAL_BAUD_RATE, 
+                    timeout=None
+                )    
+        
+        if self.gripper:
+            self.open_gripper()
+        else:
+            raise Exception("Gripper not found")
+
+    def get_all_ports(self, ):
+        return [tuple(p) for p in list(port_list.comports())]
+
+    def get_port_name(self, port):
+        return port[0]
+    
+    def open_gripper(self, ):
+        self.gripper.write(f"h".encode("utf-8"))
+        # wait_till_done()
+
+    def close_gripper(self, ):
+        self.gripper.write(f"g".encode("utf-8"))
+        # wait_till_done()
+
+    def rotate_gripper(self, angle):
+        # on degree -90:90 (its ralative to the current angle)
+        self.gripper.write(f"r{angle}".encode("utf-8"))
+        wait_till_done()
+
+    def force_gripper(self, force):
+        # int from 1 to 5000
+        self.gripper.write(f"f{force}".encode("utf-8"))
+        wait_till_done()
+    
+    def wait_till_done(self, ):
+        while 1:
+            result = self.gripper.readline().decode("utf-8")
+            print(f"Gripper: {result}")
+            if result[0:4] == "Done":
+                break
+
+    ## Delta functions
+    def wait_till_done_robot(self, ):
+        while 1:
+            result = Client.Result
+            print(f"Robot: {result}")
+            if result == "success":
+                break
+
+    def go_home(self, ):
+        Client.order("move", f"0,0,-37")
+        wait_till_done_robot()
+
+    def move(self, x, y, z):
+        Client.order("move", f"{x},{y},{z}")
+        wait_till_done_robot()
+
+    def read_forward(self, ):
+        Client.order("command", "forward")
+        robot_current_coordinate = [Client.Result[1], Client.Result[2], Client.Result[3]]
+        return robot_current_coordinate
