3689e36f679139c947249483fe2bb9a52d34c220
Arman Barghi <arman.barghi@gmail.com>
2023-09-26 15:58:49 +0330

    move_on_click renamed to main_on_click

--- main_on_click.py
+++ main_on_click.py
@@ -1,27 +1,33 @@
 import cv2
 import numpy as np
 import PySimpleGUI as psg
-import delta_manager.client as Client
 import delta_manager.camera as Camera
-import delta_manager.delta as Delta
+import delta_manager.delta_manager.DeltaManager
 
 
 def click_event(event, u, v, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(f'Pixel: ({u}, {v})')
 
+        z_obj = 0
+        # print(Delta.read_forward())
+
         [x, y, z] = Camera.calculate_robot_XYZ(
             (u, v), 
             z_obj, 
-            robot_capturing_coord
+            robot_capturing_coord=np.array([0,0,-37]),
+            offset_valid=True
         )
-        
+
         print(f'Robot: ({x:.2f}, {y:.2f}, {z:.2f})')
-        
-        Client.order("move", f'{x},{y},{z}')
+
+        Delta.move(x, y, z)
 
 
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+Delta = DeltaManager()
+Delta.connect_gripper()
+
+cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
 
 image_counter = 0
 
@@ -31,13 +37,9 @@ while True:
     # Undistort the frame
     undist_frame = Camera.undistort(frame)
     
-    # Set object x, y, z coordinates
-    z_obj  = 0
-    robot_capturing_coord = np.array([0,0,-37])
-
     # Image show
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('image', 1080, 950)
+    cv2.resizeWindow('image', 800, 600)
     cv2.setMouseCallback('image', click_event)
     cv2.imshow('image', frame)
 
@@ -49,6 +51,12 @@ while True:
         cv2.imwrite('./Images/image'+str(image_counter)+'.jpg', frame)
         print(f'image {image_counter} saved.')
         image_counter += 1
+    elif key_pressed == ord('o'):
+        Delta.open_gripper()
+    elif key_pressed == ord('c'):
+        Delta.close_gripper()
+    elif key_pressed == ord('h'): 
+        Delta.go_home()
 
 cap.release()
 cv2.destroyAllWindows()
